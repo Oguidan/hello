b3b3c286feaba31dc3334fb4f9deaed2fe43c8b3
ArchangelMichael <michaeloguidan@gmail.com>
2023-07-06 22:17:34 +0000

    Switch conditional

--- hello.go
+++ hello.go
@@ -2,6 +2,52 @@ package main
 
 import "fmt"
 
+// Compare returns an integer comparing the two byte slices,
+// lexicographically.
+// The result will be 0 if a == b, -1 if a < b, and +1 if a > b
+func Compare(a, b []byte) int {
+	for i := 0; i < len(a) && i < len(b); i++ {
+		switch {
+		case a[i] > b[i]:
+			return 1
+		case a[i] < b[i]:
+			return -1
+		}
+	}
+	switch {
+	case len(a) > len(b):
+		return 1
+	case len(a) < len(b):
+		return -1
+	}
+	return 0
+}
+
+// Defer function
+func trace(s string) string {
+	fmt.Println("entering:", s)
+	return s
+}
+
+func un(s string) {
+	fmt.Println("leaving:", s)
+}
+
+func a() {
+	defer un(trace("a"))
+	fmt.Println("in a")
+}
+
+func b() {
+	defer un(trace("b"))
+	fmt.Println("in b")
+	a()
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	// Example usage
+	a := []byte{1, 2, 3}
+	c := []byte{1, 2, 4}
+	result := Compare(a, c)
+	fmt.Println(result)
 }
