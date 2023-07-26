dc8196b365e7abed0e4783956da00b0fe8b06ffa
ArchangelMichael <michaeloguidan@gmail.com>
2023-07-26 22:48:34 +0000

    Arrays

--- hello.go
+++ hello.go
@@ -44,6 +44,13 @@ func b() {
 	a()
 }
 
+func Sum(a *[3]float64) (sum float64) {
+	for _, v := range *a {
+		sum += v
+	}
+	return
+}
+
 func main() {
 	// Example usage
 	a := []byte{1, 2, 3}
@@ -53,4 +60,9 @@ func main() {
 
 	// Defers functions
 	b()
+
+	// Arrays
+	array := [...]float64{7.0, 8.5, 9.1}
+	x := Sum(&array)
+	fmt.Println(x)
 }
