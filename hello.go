318ce02fc179a94a4b66c5dad5a22c7fd07cad4c
Michael Oguidan <michaeloguidan@gmail.com>
2024-04-08 16:30:30 +0000

    Add sort package and implement sorting for Sequence type in hello.go

--- hello.go
+++ hello.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
 )
 
 // Compare returns an integer comparing the two byte slices,
@@ -181,6 +182,42 @@ func (p *ByteSlice) Write(data []byte) (n int, err error) {
 	return len(data), nil
 }
 
+// Interfaces and other types
+// Interfaces
+
+type Sequence []int
+
+// Methods required by sort.Interface.
+func (s Sequence) Len() int {
+	return len(s)
+}
+func (s Sequence) Less(i, j int) bool {
+	return s[i] < s[j]
+}
+func (s Sequence) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+// Copy returns a copy of the Sequence.
+func (s Sequence) Copy() Sequence {
+	copy := make(Sequence, 0, len(s))
+	return append(copy, s...)
+}
+
+// Method for printing - sorts the elements before printing.
+func (s Sequence) String() string {
+	s = s.Copy() // Make a copy; don't overwrite argument.
+	sort.Sort(s)
+	str := "["
+	for i, elem := range s { // Loop is O(N²); will fix that in next example.
+		if i > 0 {
+			str += " "
+		}
+		str += fmt.Sprint(elem)
+	}
+	return str + "]"
+}
+
 func main() {
 	// Example usage
 	a := []byte{1, 2, 3}
@@ -226,10 +263,4 @@ func main() {
 	// Pointers vs. Values
 	var b ByteSlice
 	fmt.Fprintf(&b, "This hour has %d days\n", 7)
-
-	// Interfaces and other types
-	// Interfaces
-	type Sequence []int
-
-	// Methods
 }
